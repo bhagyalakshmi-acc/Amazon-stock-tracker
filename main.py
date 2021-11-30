1ec2e77542e097e2bdb9941d0fb010e43e642cb9
bhagyalakshmi-acc <94637376+bhagyalakshmi-acc@users.noreply.github.com>
2021-11-30 22:47:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@
 #In mail I need Link-Instock
 #automatically per hour
 import time
-
 import requests
 from bs4 import BeautifulSoup
 import smtplib
@@ -24,20 +23,18 @@ def check_avail():
     if(availability=='In stock.'):
         send_mail()
 
-
-
 def send_mail():
     server = smtplib.SMTP('smtp.gmail.com',587)
     server.ehlo()
     server.starttls()
     server.ehlo()
 
-    server.login('bhagya.workacc@gmail.com','jgjeqamrkcndebdn')
+    server.login(' @gmail.com',' ')
 
     subject = 'Product is now available in stock'
     body    = 'Click the link https://www.amazon.in/Smart-AMOLED-Display-Battery-Resistant/dp/B08TBKJ5LZ/ref=sr_1_1?keywords=mi%2Bfit%2Bband%2B5&qid=1637503034&qsid=257-8246201-5785306&sr=8-1&sres=B08GXC2NTX%2CB08KQLK2RC%2CB09J2C7YDH%2CB08GMP6VRS%2CB09G6QV45F%2CB09FYZGB5T%2CB09GP8QZZQ%2CB09F2HHDFT%2CB09HC7KQNQ%2CB09J4WVQ8C%2CB09FFKH5Y2%2CB09FPYY7B2%2CB091G3FLL7%2CB09LR1HWR6%2CB09KQ77S9Y%2CB07XY9BZPM%2CB08W49129G%2CB08R91SZSN%2CB09FD6ZYTD%2CB09FF1PLFM&srpt=WEARABLE_COMPUTER&th=1 '
     msg = f"Subject:{subject}\n\n{body}"
-    server.sendmail('bhagya.workacc@gmail.com','bhagya.workacc@gmail.com',msg)
+    server.sendmail('  @gmail.com','  @gmail.com',msg)
 
     server.quit()
 
